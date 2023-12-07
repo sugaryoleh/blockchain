ee44e13ee32b80a4391d1c5e0881402142163f52
suagryoleh <suagryoleh@gmail.com>
2023-12-08 01:58:46 +0200

    removed unnecessary files

--- SMS/sms.py
+++ SMS/sms.py
@@ -15,12 +15,12 @@ class SMSManager:
         self.client = Client(self.account_sid, self.auth_token)
 
     def send_message(self, text: str, to: str) -> None:
-        # message = self.client.messages.create(
-        #     from_='+12676139604',
-        #     body=text,
-        #     to=to
-        # )
-        print("{} sent to {}".format(text, '+380502265769'))
+        message = self.client.messages.create(
+            from_='+12676139604',
+            body=text,
+            to=to
+        )
+        print("{} sent to {}".format(text, '+380681374435'))
 
     def create_new_account_message(self, account) -> None:
         message = "Welcome {}! Your account has been successfully created!".format(account.user.username)
